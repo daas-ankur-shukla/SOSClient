14a1c5b7c38ae5a9c68316c3541e7e36b89cf9a2
daas-ankur-shukla <work.ankurshukla@gmail.com>
2018-04-05 20:40:56 +0530

    working on describeStation

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,17 +1,22 @@
 // Variable definitions
 var map
 var ndbc_sos = 'https://sdf.ndbc.noaa.gov/sos/server.php?request=GetCapabilities&service=SOS'
-var local_sos= 'https://'
+var describeStationURL = 'https://sdf.ndbc.noaa.gov/sos/server.php?request=DescribeSensor&service=SOS&version=1.0.0&outputformat=text/xml;subtype=%22sensorML/1.0.1%22&procedure=urn:ioos:station:wmo:'
+var local_sos = 'https://'
 var GetCapabilitiesXML
 var markerJSON
 var stationCount
-var markers = L.markerClusterGroup();
+var stationArray = []
+var stationGroups = L.markerClusterGroup({chunkedLoading: true});
 
-var map = L.map('map').setView([19.228825, 72.854110], 1.5);
+var stationMarker
+var map = L.map('map').setView([
+  19.228825, 72.854110
+], 1.5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 10,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 10
 }).addTo(map);
 
 function StringToXMLDom(string) {
@@ -27,6 +32,13 @@ function StringToXMLDom(string) {
   return xmlDoc;
 }
 
+function describeStation(stationXML) {
+  console.log(stationXML.children[0].children[0].children[0])
+  var stationInfo = stationXML.children[0].children[0].children[0];
+  
+  return 'hi'
+}
+
 var co;
 
 $.ajax({
@@ -34,25 +46,44 @@ $.ajax({
   dataType: 'text',
   success: function(result) {
     // console.log(result);
-    GetCapabilitiesXML=StringToXMLDom(result);
+    GetCapabilitiesXML = StringToXMLDom(result);
 
     // console.log(GetCapabilitiesXML)
     var capabilities = GetCapabilitiesXML.getElementsByTagName('sos:Capabilities')[0]
     // console.log(capabilities)
     var observationOfferingList = capabilities.children[3].children[0].children;
-    // console.log(observationOfferingList)
-    stationCount=observationOfferingList.length
+    console.log(observationOfferingList[2])
+    stationCount = observationOfferingList.length
     var stationCoordinates
     var stationDetails
     for (i = 1; i < stationCount; i++) {
-      stationDetails=observationOfferingList[i].children[1].innerHTML;
-      stationCoordinates=observationOfferingList[i].children[3].children[0].children[0].innerHTML.split(' ');
-      var marker=L.marker([stationCoordinates[0], stationCoordinates[1]]);
-      marker.bindPopup(stationDetails);
-      markers.addLayer(marker);
-      map.addLayer(markers);
+      var stationHTML = '<p>Station Description: ' + observationOfferingList[i].children[0].innerHTML + '</p>';
+      var stationID = observationOfferingList[i].children[1].innerHTML.split(':').pop()
+      stationHTML += '<p>StationID: ' + stationID + '</p>';
+      stationCoordinates = observationOfferingList[i].children[3].children[0].children[0].innerHTML.split(' ');
+      stationMarker = L.marker([stationCoordinates[0], stationCoordinates[1]], {stationID: stationID});
+      stationMarker.bindPopup('<p>Please wait, I am looking for my SensorML</p>');
+      stationMarker.on('click', function(e) {
+        var popup = e.target.getPopup();
+        var id = this.options.stationID
+        setTimeout(function() {
+          // console.log(e.target, e.target.options.stationID)
+          $.get(describeStationURL+id).done(function(data) {
+            // console.log(data)
+            stationData = describeStation(data);
+            popup.setContent(stationData);
+            popup.update();
+          });
+          //your code to be executed after 1 second
+        }, 150);
+
+      })
+      stationArray.push({marker:stationMarker, detail:stationHTML});
+      stationGroups.addLayer(stationMarker);
     }
 
+    // console.log('adding to layer')
+    map.addLayer(stationGroups);
   },
   error: function(xhr, staus, error) {
     console.log('error in ajax', status, error);
