0b82de3884a459242e9c3923d13a95794a9a9e44
daas-ankur-shukla <work.ankurshukla@gmail.com>
2018-04-16 04:07:03 +0530

    temporal filter skeleton

--- controller.js
+++ controller.js
@@ -123,8 +123,32 @@ function spatialFiltering(state) {
 };
 
 function temporalFiltering(state) {
-  if (state) {} else {}
+  if (state) {
+
+  }else {}
 };
+
+L.Control.Sample = L.Control.extend({
+  options: {
+    // topright, topleft, bottomleft, bottomright
+    position: 'topright'
+  },
+  initialize: function (options) {
+    // constructor
+  },
+  onAdd: function (map) {
+    // happens after added to map
+  },
+  onRemove: function (map) {
+    // when removed
+  }
+});
+
+L.control.sample = function(id, options) {
+  return new L.Control.Sample(id, options);
+}
+
+
 
 function propertyFiltering(prop) {
   if(prop!='RESET') {
